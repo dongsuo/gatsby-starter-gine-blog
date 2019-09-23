d6714d032d54a443db465caf048627764ac8bc97
东索 <xuxiaofei1.0@outlook.com>
2019-09-23 18:31:47 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/dongsuo/02dfc61cc3ed4985b0530b10c5549619?v=8b4101da602546f9b145302366f1ea4b"
+                configTable: "https://www.notion.so/dongsuo/050017af19124d999d5828e388f08979?v=4afbd65fb6974d89a63f081b7ddccb26"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/dongsuo/050017af19124d999d5828e388f08979?v=4afbd65fb6974d89a63f081b7ddccb26"
+                configTable: "https://www.notion.so/dongsuo/02dfc61cc3ed4985b0530b10c5549619?v=8b4101da602546f9b145302366f1ea4b"
             }
         }
     ],
